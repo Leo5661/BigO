865d4b31d8ffce900fe5d0bf07fe0b1f826fd239
leocoder <singhra66@gmail.com>
2024-04-14 13:44:51 +0530

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div className="h-screen w-screen overflow-hidden text-foreground bg-background">
     <NavBar />
     <div className="flex flex-col flex-grow h-full justify-center items-center">
-      <div className="">jai shree ram</div>
       <Link to="/taskA" className="border rounded-md px-4 py-2">Link A</Link>
       <Link to="/taskB" className="border rounded-md px-4 py-2">Link B</Link>
     </div>
